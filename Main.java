d5bc5d8ab12f02b206c54ea3ab7b5d4e492d35cf
Moise1997 <63591685+Moise1997@users.noreply.github.com>
2020-04-13 15:46:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+public class Main {
+
+    public static void main(String [] args) {
+        System.out.println("Hello");
+        System.out.println("Moise");
+
+        byte cineva = 100;
+        short oricine = 134;
+        int ceva = 12412;
+        int rezultat = cineva + oricine;
+        System.out.println(rezultat);
+
+        long money = 200000;
+        int bills = 100000;
+        long moneyLeftForSpendOnCandy = money / bills;
+        System.out.println(moneyLeftForSpendOnCandy);
+
+        byte undeva = -5;
+        byte altceva = 8;
+        byte orice = 6;
+        int comoara = ceva + altceva * orice;
+        System.out.println(comoara);
+
+        byte ana = 55;
+        byte are = 9;
+        byte mere = 9;
+        System.out.println((ana+are) % mere);
+
+        byte ciresica = 20;
+        byte avea = 3;
+        byte capsuni = 5;
+        byte ciresel = 8;
+        System.out.println(ciresica +- are * mere / ciresel);
+
+        byte a = 5;
+        byte aa = 15;
+        byte b = 3;
+        byte bb = 2;
+        byte c = 8;
+        byte cc = 3;
+        System.out.println( a + aa / b * bb - c % cc);
+
+
+
+
+
+
+
+
+    }
+}
